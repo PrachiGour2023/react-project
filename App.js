79fbc869f938fae34ad405a32cc2736deb976328
PrachiGour2023 <prachi.gour602@gmail.com>
2023-05-26 15:19:43 +0530

    new changes

--- App.js
+++ App.js
@@ -1,29 +1,45 @@
 /*
-* HMR - Hot Module Reloading 
-* File Watcher Algorithm - C++
-* Bundling
-* Minify
-* Cleaning our code
-* Dev and Production build
-* Super fast build program 
-* Images Optimization
-* Caching while developement
-* Compression
-* Compatible with older version of browser / polyfill
-* Enable HTTPS on developement
-* Port number
-*/
+ * HMR - Hot Module Replacement
+ * File Watcher Algorithm - C++
+ * Bundling
+ * Minify
+ * Cleaning our code
+ * Dev and Production build
+ * Super fast build program
+ * Images Optimization
+ * Caching while developement
+ * Compression
+ * Compatible with older version of browser / polyfill
+ * Enable HTTPS on developement
+ * Port number
+ * Consistent Hashing Algorithm
+ * Zero Configuration
+ * Transitive Dependecies
+ *
+ *
+ */
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", {
-    id: 'head'
-}, "Namaste React");
-const div = React.createElement("div", { 
-    className: "container"
-},[ 
-React.createElement("p", {}, "Paragraph Tag"),
-React.createElement("span", {}, "Span Tag")
-]);
+const heading = React.createElement(
+  "h1",
+  {
+    key: 1,
+    id: "head",
+  },
+  "Namaste React"
+);
+const div = React.createElement(
+  "div",
+  {
+    key : 2,
+    className: "container",
+  },
+  [
+    React.createElement("p", { key: 1 }, "Paragraph Tag"),
+    React.createElement("span", { key: 2 }, "Span Tag"),
+  ]
+);
+console.log(div);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render([heading, div]);
