0ecf098ba90004afd2ef1f5f3f7bccd697aceb53
PrachiGour2023 <prachi.gour602@gmail.com>
2023-05-30 15:00:35 +0530

    Introduced State Variable in react

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,7 +1,19 @@
 import { restaurantList } from "../../constant";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [restaurants, setRestaurants] = useState(restaurantList);
+
+  const filterData = () => {
+    const lowerString = searchInput.toLowerCase();
+    let filteredData = restaurants.filter((restaurant) => {
+      return restaurant.name.toLowerCase().includes(lowerString);
+    });
+    setRestaurants(filteredData);
+  };
+
   return (
     <>
       <div className="search-container">
@@ -10,11 +22,15 @@ const Body = () => {
           name="search"
           placeholder="Search Here...."
           className="input-search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
         />
-        <button className="btn-search">Search</button>
+        <button className="btn-search" onClick={filterData}>
+          Search
+        </button>
       </div>
       <div className="restaurant-card">
-        {restaurantList.map((restaurant) => {
+        {restaurants.map((restaurant) => {
           return <RestaurantCard {...restaurant} key={restaurant.id} />;
         })}
       </div>
